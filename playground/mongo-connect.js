8c9e824b480186bf664398301915635535fc04a3
Romil Sikka <romiilsikka@gmail.com>
2018-04-28 00:28:40 +0530

    0.0.2

--- playground/mongo-connect.js
+++
@@ -1,9 +0,0 @@
-const MongoClient = require('mongodb').MongoClient;
-
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
-  if (err) {
-    return console.log('Database connection Error');
-  }
-  console.log('Connected to Database');
-  client.close();
-});
